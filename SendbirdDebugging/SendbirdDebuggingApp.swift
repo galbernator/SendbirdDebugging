0724ad7af0d8ab957d6e2b5ebe2e8dbba1d95317
Steve Galbraith <galbernator@gmail.com>
2024-02-07 09:43:53 -0700

    Initial Commit

---
+++ SendbirdDebugging/SendbirdDebuggingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SendbirdDebuggingApp.swift
+//  SendbirdDebugging
+//
+//  Created by Steve Galbraith on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SendbirdDebuggingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
